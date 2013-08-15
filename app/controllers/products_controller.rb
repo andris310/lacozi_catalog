b3fb2dc8e93272a9b22aa95575b0ce54d058d724
andris310 <andris310@gmail.com>
2013-08-14 23:40:02 -0700

    Some css

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
   end
 
   def decorative_pillows
-    @pillows = Product.where(:category_id => 1)
+    @pillows = Product.where(:category.name => 'decorative pillows')
     respond_to do |format|
       format.html
       format.json { render json: @pillows }
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
 
 
   def duvet_sets
-    @duvet_sets = Product.where(:category => 'duvet set')
+    @duvet_sets = Product.where(:category => 'duvet cover sets')
     respond_to do |format|
       format.html
       format.json { render json: @duvet_sets }
