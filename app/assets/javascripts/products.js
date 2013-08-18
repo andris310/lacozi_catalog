942bd4e28f9d604679602402299fa663d0b44283
andris310 <andris310@gmail.com>
2013-08-18 15:21:29 -0700

    iscroll

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,37 +1,3 @@
-// getItems = (link, cssid) ->
-//   $.ajax
-//     url: link
-//     type: "get"
-//     dataType: "json"
-//     success: (results) ->
-//       list = $(cssid)
-//       $(results).each (index, result) ->
-//         item = $("<li><img src='" + result["picture"]["thumb"]["url"] + "'></li>")
-//         list.append item
-//         item.attr('item_id', result["id"])
-
-
-// $(document).ready ->
-//   $('#dec-pillows').load(getItems('/decorative-pillows.json', '#dec-pillows'))
-//   $('#duvet-sets').load(getItems('/duvet-sets.json', '#duvet-sets'))
-
-
-  // $('.list').on('click', 'li', ->
-  //   info = $('#product-data')
-  //   li = $(this)
-  //   itemId = li.attr('item_id')
-  //   info.html('')
-  //   $.ajax
-  //     url: '/product-details.json'
-  //     data: q: info
-  //     debugger
-  //     type: 'get'
-  //     dataType: 'json'
-  //     success: (result) ->
-  //       info.append('some text here')
-  //       console.log(result)
-  //   )
-
 var myScroll;
 
 
@@ -69,10 +35,13 @@ function getItemDetails(itemId) {
 }
 
 function slider() {
-        myAppShot = new iScroll('wrapper', {
+  setTimeout(function() {
+    myAppShot = new iScroll('wrapper', {
         checkDOMChanges: true
      });
-  document.addEventListener('DOMContentLoaded', loaded, false);
+  }, 100);
+
+  document.addEventListener('DOMContentLoaded', slider, false);
 }
 
 
@@ -102,12 +71,6 @@ $(document).ready(function() {
     var li = $(this);
     var itemId = li.attr('item_id');
     getItemDetails(itemId);
+    slider();
   });
-
-  slider();
-      // myScroll = new iScroll('wrapper', {
-      //   checkDOMChanges: true
-      // });
-
-// document.addEventListener('DOMContentLoaded', loaded, false);
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,13 +17,13 @@ class ProductsController < ApplicationController
   end
 
   def decorative_pillows
-    # @pillows = Product.where(:category_id => [4, 5, 9, 8])
-    @pillows = []
-    Product.all.each do |prod|
-      if prod.category.name.downcase.include? 'pillow'
-        @pillows << prod
-      end
-    end
+    @pillows = Product.all
+    # @pillows = []
+    # Product.all.each do |prod|
+    #   if prod.category.name.downcase.include? 'pillow'
+    #     @pillows << prod
+    #   end
+    # end
     respond_to do |format|
       format.html
       format.json { render json: @pillows }
